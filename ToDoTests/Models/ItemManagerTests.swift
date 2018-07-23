a6ef8bec4c3c6797cdd28cd75a1e68046107eb23
Alexandr Pavlov <alexandr.pavlov@outlook.com>
2018-07-23 15:07:30 +0300

    Added test for Models and ItemManager

--- ToDoTests/Models/ItemManagerTests.swift
+++ ToDoTests/Models/ItemManagerTests.swift
@@ -3,16 +3,80 @@ import XCTest
 
 class ItemManagerTests: XCTestCase {
     
+    //sut = System Under Test
+    var sut: ItemManager!
+    
     override func setUp() {
         super.setUp()
+        sut = ItemManager()
     }
     
     override func tearDown() {
         super.tearDown()
+        sut = nil
     }
     
     func test_ToDoCount_Initially_IsZero() {
-        //sut = System Under Test
-        let sut = ItemManager()
+        XCTAssertEqual(sut.toDoCount, 0)
+    }
+    
+    func test_DoneCount_Initially_IsZero() {
+        XCTAssertEqual(sut.doneCount, 0)
+    }
+    
+    func test_AddItem_IncreasesToDoCountToOne() {
+        sut.add(ToDoItem(title: ""))
+        XCTAssertEqual(sut.toDoCount, 1)
+    }
+    
+    func test_ItemAt_ReturnsAddedItem() {
+        let item = ToDoItem(title: "Foo")
+        sut.add(item)
+        let returnedItem = sut.item(at: 0)
+        XCTAssertEqual(returnedItem.title, item.title)
+    }
+    
+    func test_CheckItemAt_ChangesCounts() {
+        sut.add(ToDoItem(title: ""))
+        sut.checkItem(at: 0)
+        XCTAssertEqual(sut.toDoCount, 0)
+        XCTAssertEqual(sut.doneCount, 1)
+    }
+    
+    func test_CheckItemAt_RemovesItFromToDoItems() {
+        let firstItem = ToDoItem(title: "First Item")
+        let secondItem = ToDoItem(title: "Second Item")
+        sut.add(firstItem)
+        sut.add(secondItem)
+        sut.checkItem(at: 0)
+        
+        XCTAssertEqual(sut.item(at: 0).title, "Second Item")
+    }
+    
+    func test_DoneItemAt_ReturnsCheckedItem() {
+        let item = ToDoItem(title: "Foo")
+        sut.add(item)
+        sut.checkItem(at: 0)
+        let returnedItem = sut.doneItem(at: 0)
+        XCTAssertEqual(returnedItem.title, item.title)
+    }
+    
+    func test_RemoveAll_ResultsInCountsBeZero() {
+        
+        sut.add(ToDoItem(title: "Foo"))
+        sut.add(ToDoItem(title: "Bar"))
+        sut.checkItem(at: 0)
+        XCTAssertEqual(sut.toDoCount, 1)
+        XCTAssertEqual(sut.doneCount, 1)
+        
+        sut.removeAll()
+        XCTAssertEqual(sut.toDoCount, 0)
+        XCTAssertEqual(sut.doneCount, 0)
+    }
+    
+    func test_Add_WhenItemIsAlreadyAdded_DoesNotIncreaseCount() {
+        sut.add(ToDoItem(title: "Foo"))
+        sut.add(ToDoItem(title: "Foo"))
+        XCTAssertEqual(sut.toDoCount, 1)
     }
 }
--- ToDoTests/Models/LocationTests.swift
+++ ToDoTests/Models/LocationTests.swift
@@ -24,4 +24,59 @@ class LocationTests: XCTestCase {
         XCTAssertEqual(location.coordinate?.latitude, coordinate.latitude)
         XCTAssertEqual(location.coordinate?.longitude, coordinate.longitude)
     }
+    
+    func test_EqualLocations_AreEqual() {
+        let first = Location(name: "Foo")
+        let second = Location(name: "Foo")
+        XCTAssertEqual(first, second)
+    }
+    
+    func test_Locations_WhenLatitudeDiffers_AreNotEqual() {
+        assertLocationNotEqualWith(firstName: "Foo",
+                                   firstLongLat: (latitude: 1.0, longitude: 0.0),
+                                   secondName: "Foo",
+                                   secondLongLat: (latitude: 0.0, longitude: 0.0))
+    }
+
+    func test_Locations_WhenLongitudeDiffers_AreNotEqual() {
+        assertLocationNotEqualWith(firstName: "Foo",
+                                   firstLongLat: (latitude: 0.0, longitude: 1.0),
+                                   secondName: "Foo",
+                                   secondLongLat: (latitude: 0.0, longitude: 0.0))
+    }
+    
+    func test_Locations_WhenOnlyOneHasCoordinate_AreNotEqual() {
+        assertLocationNotEqualWith(firstName: "Foo",
+                                   firstLongLat: (0.0, 0.0),
+                                   secondName: "Foo",
+                                   secondLongLat: nil)
+    }
+    
+    func test_Locations_WhenNamesDiffer_AreNotEqual() {
+        assertLocationNotEqualWith(firstName: "Foo",
+                                   firstLongLat: nil,
+                                   secondName: "Bar",
+                                   secondLongLat: nil)
+    }
+    
+    func assertLocationNotEqualWith(firstName: String,
+                                    firstLongLat: (latitude: Double, longitude: Double)?,
+                                    secondName: String,
+                                    secondLongLat: (latitude: Double, longitude: Double)?,
+                                    line: UInt = #line) {
+        
+        var firstCoord: CLLocationCoordinate2D? = nil
+        if let firstLongLat = firstLongLat {
+            firstCoord = CLLocationCoordinate2D(latitude: firstLongLat.latitude, longitude: firstLongLat.longitude)
+        }
+        let firstLocation = Location(name: firstName, coordinate: firstCoord)
+        
+        var secondCoord: CLLocationCoordinate2D? = nil
+        if let secondLongLat = secondLongLat {
+            secondCoord = CLLocationCoordinate2D(latitude: secondLongLat.latitude, longitude: secondLongLat.longitude)
+        }
+        let secondLocation = Location(name: secondName, coordinate: secondCoord)
+        
+        XCTAssertNotEqual(firstLocation, secondLocation, line: line)
+    }
 }
